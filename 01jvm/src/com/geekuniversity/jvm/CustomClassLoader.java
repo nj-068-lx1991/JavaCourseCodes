4eab83c61a48694e82615fc027e101901a2c873e
nj_lx_068 <m19951937965@163.com>
2021-11-07 19:55:59 +0800

    01jvm 自定义类加载器

---
+++ 01jvm/src/com/geekuniversity/jvm/CustomClassLoader.java
@@ -0,0 +1,45 @@
+package com.geekuniversity.jvm;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * 自定义一个 Classloader，加载一个 Hello.xlass 文件，执行 hello 方法，
+ * 此文件内容是一个 Hello.class 文件所有字节（x=255-x）处理后的文件。文件群里提供。
+ *
+ * @author nj_lx_068
+ * @date 2021/11/07
+ */
+public class CustomClassLoader extends ClassLoader {
+
+    public static void main(String[] args) {
+        try {
+            Class<?> helloClass = new CustomClassLoader().findClass("Hello");
+            Method helloMethod = helloClass.getMethod("hello");
+            helloMethod.invoke(helloClass.newInstance());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+
+        // 获取 Hello.xclass文件路径，与项目代码处于同级目录
+        Path path = Paths.get("Hello.xlass");
+        byte[] helloBase64 = new byte[0];
+        try {
+            helloBase64 = Files.readAllBytes(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < helloBase64.length; i++) {
+            helloBase64[i] = (byte) (255 - helloBase64[i]);
+        }
+        return defineClass(name, helloBase64, 0, helloBase64.length);
+    }
+
+}
---
+++ 01jvm/src/com/geekuniversity/jvm/Main.java
@@ -0,0 +1,8 @@
+package com.geekuniversity.jvm;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
